d2d301319a70df39f7f4f455df820a5ae49d0918
RocYan98 <rocyan98@gmail.com>
2023-09-02 13:35:23 +0000

    Deploying to gh-pages from @ RocYan98/RocYan98.github.io@9bc74858d454a7f816360e1d7a0ab598bba52cd5 🚀

--- assets/pageview-1f0dfae6.js
+++
@@ -1,1 +0,0 @@
-import{M as e}from"./app-4f7a4430.js";const a=()=>e({serverURL:"https://blog-comments-rkyleais0-rocyan98.vercel.app"});export{a as updatePageview};
---
+++ assets/pageview-f39fdd70.js
@@ -0,0 +1,1 @@
+import{M as e}from"./app-485f4dee.js";const t=()=>e({serverURL:"http://comment.rocyan.top"});export{t as updatePageview};
