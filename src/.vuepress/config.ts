9510cf9d8d03fa0516632b700a479c1155160074
Yan <rocyan98@gmail.com>
2023-11-22 23:03:10 +0800

    deploy

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -17,7 +17,8 @@ export default defineUserConfig({
 
   head: [
     [
-      'meta', {name: "baidu-site-verification", content: "codeva-zvAJUNXpbz"}
+      'meta', {name: "baidu-site-verification", content: "codeva-zvAJUNXpbz"},
+      'meta', {name: "baidu-site-verification", content: "codeva-Ur6mE6jBXO"}
     ],
   ],
 
