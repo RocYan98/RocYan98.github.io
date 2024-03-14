72549f6ce13f807747710b80ac3ed272042f423a
Yan <rocyan98@gmail.com>
2024-03-14 21:24:00 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 set -e
 
-git add .
-git commit -m "deploy"
-git push -f git@github.com:RocYan98/RocYan98.github.io.git main
+LANG=en_GB git add .
+LANG=en_GB git commit -m "deploy"
+LANG=en_GB git push -f git@github.com:RocYan98/RocYan98.github.io.git main
