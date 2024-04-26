386699842a375ad5280347e7082c99d364c93eb5
Yan <rocyan98@gmail.com>
2024-04-26 16:07:43 +0800

    deploy

--- deploy_remote.sh
+++ deploy_remote.sh
@@ -5,5 +5,5 @@ LANG=en_GB git commit -m "deploy"
 LANG=en_GB git push -f git@github.com:RocYan98/RocYan98.github.io.git main
 
 npm run docs:build
-scp -q -r /Users/Yan/Desktop/vuepress/src/.vuepress yan@macmini:
+scp -r -P 9995 /Users/Yan/Desktop/vuepress/src/.vuepress yan@rocyan.cn:
 echo scp finish!
