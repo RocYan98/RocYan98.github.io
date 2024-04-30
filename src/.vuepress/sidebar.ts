55fa2a1342caec4a05036f4251a5ba4f88e4b88a
Yan <rocyan98@gmail.com>
2024-04-30 21:39:27 +0800

    deploy

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -35,7 +35,8 @@ export default sidebar({
                             prefix: "GAMES101/",
                             collapsible: true,
                             children: "structure"
-                        }
+                        },
+                        'coordinate.md'
                     ]
                 },
                 {
